a4ee0d7e9515711406e5930a39658add1ab1cedd
wkitip <153873017+wkitip@users.noreply.github.com>
2025-04-28 22:26:11 -0700

    Add files via upload

--- src/libraryMember/Member.java
+++ src/libraryMember/Member.java
@@ -1,4 +1,3 @@
-package libraryMember;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,19 +33,45 @@ public class Member {
 		//REMEMBER THE MAIN WAY TO FIND A MEMBER IS WITH THE USERID
 	}
 	
-	//get userID
+	// Constructor ONLY for loading already existing members from a file since ID is randomly generated
+	// name, userID, strikes, accountHold
+	public Member(String n, String uID, String s, String a) {
+		name = n;
+		userID = uID;
+		strikes = Integer.valueOf(s);
+		accountHold = Boolean.valueOf(a);
+	}
+	
+	// get userID
 	public String getUserID() {
 	    return this.userID;
 	}
 	
+	public String getName() {
+		return name;
+	}
+	
+	// Returns a string with format Name,userID,Strikes,accountHold 
+	// Example: Ricky Ip,RIC412,0,false
+	public String toString() {
+		return (name + "," + userID + "," + String.valueOf(strikes) + "," + String.valueOf(accountHold));
+	}
+	
+	/*
 	//get account standing
 	public String getStanding() {
 		String standingOutput = "Member account " + this.userID + " has " + this.strikes + " strikes.\n";
 		return standingOutput;
+	}*/
+	
+	// Returns the # of strikes as a string
+	public String getStrikes() {
+		return String.valueOf(strikes);
 	}
 	
-	//get account hold status
+	// Returns true/false of the account hold as a string
 	public String getAccountHold() {
+		/*
 		if (accountHold == true) {
 			String accountHoldStatus = "Account " + this.userID + " is on hold.\n";
 			return accountHoldStatus;
@@ -54,7 +79,9 @@ public class Member {
 		else {
 			String accountHoldStatus = "Account " + this.userID + " is not on hold.\n";
 			return accountHoldStatus;
-		}
+		}*/
+		
+		return String.valueOf(accountHold);
 	}
 	
 	//set account hold status
@@ -72,28 +99,18 @@ public class Member {
 	
 	//add items to accounts Checked out Items
 	public void checkoutItem() {
-		//takes itemID and puts that under the account, itemID is used to find item info (author, title etc)
-		Item newItem = new Item();
-		//add the new item to the list of checked out items
-		this.checkedOutItems.add(newItem);
+		
 	}
 	
 	//add items to accounts reserved Items
-		public void reserveItem() {
-			//takes itemID and puts that under the account, itemID is used to find item info (author, title etc)
-			Item newItem = new Item();
-			//add the new item to the list of checked out items
-			this.reservedItems.add(newItem);
-		}
+	public void reserveItem() {
+
+	}
 	
 	
 	//get account checked out items
 	public String getCheckedItems() {
-	//for all items in checkedOutItems, iterate and add each item to allcheckeditems string to send
-	String allCheckedOutItems = "";
-		for (Item ci : this.checkedOutItems) {
-			allCheckedOutItems += ci.getItem() + "\n";
-		}
-		return allCheckedOutItems;
+		return "";
 	}
+	
 }
