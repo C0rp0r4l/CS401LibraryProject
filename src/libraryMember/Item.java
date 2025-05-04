d6d89ec14adbb6cb16418e022f310fe661335718
Josh Charlesworth <Jaccharlesworth@gmail.com>
2025-05-04 13:08:27 -0700

    Modify Item/ItemList
    Had to modify ItemList to contain getIArray and getNumItems for the location class sort method.

--- src/libraryMember/Item.java
+++ src/libraryMember/Item.java
@@ -3,99 +3,89 @@ package libraryMember;
 import java.io.Serializable;
 import java.util.Random;
 
-public class Item implements Serializable{
-	static private int count = 0;
-	private String itemID;
-	private String title;
-	private String year;
-	private String author;
-	private String[] memberList;
-	private Integer numMember;
-	private Integer quantity;
-	
-	public Item(String t, String y, String a, Integer q) {
-		title = t;
-		year = y;
-		author = a;
-		quantity = q;
-		memberList = new String[100];
-		
-		// An issue with just using random is that it's posssible for two items to get the same ID
-		// by appending a count to it this assures even if the first X digits are the same the last two are always
-		// different
-		String temp = t.substring(0, 2) + y.substring(0,4) + a.substring(0,2);
-		Random rand = new Random();
-		int randomDigits = rand.nextInt(30000) + 10000;
-		itemID = temp + randomDigits + count;
-		count++;
-	}
-	
-	// Constructor ONLY for loading items into an item list
-	public Item(String i, String t, String y, String a, Integer q) {
-		title = t;
-		year = y;
-		author = a;
-		quantity = q;
-		memberList = new String[100];
-	}
-	
-	// Check if the number of owners is higher than the available quantity
-	// If it is return false, if not return true (indicating a succesful operation)
-	public Boolean addMember(String id) {
-		if(memberList.length >= quantity) {
-			return false;
-		}
-		else {
-			memberList[numMember] = id;
-			return true;
-		}
-	}
+public class Item implements Serializable {
+    static private int count = 0;
+    private String itemID;
+    private String title;
+    private String year;
+    private String author;
+    private String[] memberList;
+    private Integer numMember;
+    private Integer quantity;
+    
+    public Item(String t, String y, String a, Integer q) {
+        title = t;
+        year = y;
+        author = a;
+        quantity = q;
+        memberList = new String[100];
+        
+        String temp = t.substring(0, 2) + y.substring(0,4) + a.substring(0,2);
+        Random rand = new Random();
+        int randomDigits = rand.nextInt(30000) + 10000;
+        itemID = temp + randomDigits + count;
+        count++;
+    }
+    
+    public Item(String i, String t, String y, String a, Integer q) {
+        title = t;
+        year = y;
+        author = a;
+        quantity = q;
+        memberList = new String[100];
+    }
+    
+    public Boolean addMember(String id) {
+        if(memberList.length >= quantity) {
+            return false;
+        }
+        else {
+            memberList[numMember] = id;
+            return true;
+        }
+    }
 
-	public Boolean removeMember(String id){
-		for(int i = 0; i < numMember; i++) {
-			if(memberList[i].compareTo(id) == 0){
-				memberList[i] = memberList[numMember];
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	public Boolean isMemberHere(String id) {
-		for(int i = 0; i < numMember; i++) { 
-			if(memberList[i].compareTo(id) == 0) {
-				return true;
-			}
-		}
-		return false;
-	}
+    public Boolean removeMember(String id){
+        for(int i = 0; i < numMember; i++) {
+            if(memberList[i].compareTo(id) == 0){
+                memberList[i] = memberList[numMember];
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public Boolean isMemberHere(String id) {
+        for(int i = 0; i < numMember; i++) { 
+            if(memberList[i].compareTo(id) == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	// Print out the attributes seperated by comma and the number of owners + list of members
-	// number of owners is important because we need to somehow know the number of strings to parse
-	// while saving or loading.
-	public String toString() {
-		String temp = itemID + "," + title + "," + year + "," + author + "," + quantity + "," + numMember;
-		for(int i = 0; i < memberList.length; i++) {
-			String list = "," + memberList[i];
-			temp.concat(list);
-		}
-		return temp;
-	}
-	
-	public String getID() {
-		return itemID;
-	}
-	
-	public String getTitle() {
-		return title;
-	}
-	
-	public String getYear() {
-		return year;
-	}
-	
-	public String getAuthor() {
-		return author;
-	}
-	
+    public String toString() {
+        String temp = itemID + "," + title + "," + year + "," + author + "," + quantity + "," + numMember;
+        for(int i = 0; i < memberList.length; i++) {
+            String list = "," + memberList[i];
+            temp.concat(list);
+        }
+        return temp;
+    }
+    
+    public String getID() {
+        return itemID;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public String getYear() {
+        return year;
+    }
+    
+    public String getAuthor() {
+        return author;
+    }
 }
--- src/libraryMember/ItemList.java
+++ src/libraryMember/ItemList.java
@@ -1,212 +1,197 @@
 package libraryMember;
 import java.io.*;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ItemList {
-	private Integer numItems;
-	private Item[] iArray;
-	private String sourceName;
-	private itemListType type;
-	private boolean modified = false;
-	
-	
-	// Two types of item lists, one will be for keeping track of rentals, the other for reservations
-	public ItemList(itemListType t) {
-		numItems = 0;
-		iArray = new Item[1000];
-		type = t;
-		if(type == itemListType.Rental) {
-			sourceName = "itemRentalList";
-		}
-		else if(type == itemListType.Reservation) {
-			sourceName = "itemReservationList";
-		}
-		else if(type == itemListType.Library) {
-			sourceName = "itemsInLibrary";
-		}
-	}
+    private Integer numItems;
+    private Item[] iArray;
+    private String sourceName;
+    private itemListType type;
+    private boolean modified = false;
+    
+    public ItemList(itemListType t) {
+        numItems = 0;
+        iArray = new Item[1000];
+        type = t;
+        if(type == itemListType.Rental) {
+            sourceName = "itemRentalList";
+        }
+        else if(type == itemListType.Reservation) {
+            sourceName = "itemReservationList";
+        }
+        else if(type == itemListType.Library) {
+            sourceName = "itemsInLibrary";
+        }
+    }
 
-	// If member isn't here then an empty string should be returned
-	public String listOfItemRentedByMember(String id) {
-		String list = "";
+    public String listOfItemRentedByMember(String id) {
+        String list = "";
+        for(int i = 0; i < numItems; i++) {
+            if(iArray[i].isMemberHere(id) == true) {
+                list.concat(iArray[i].getID());
+            }
+        }
+        return list;
+    }
+    
+    public String toString() {
+        String list = "";
+        for(int i = 0; i < numItems; i++) {
+            String temp = iArray[i].toString() + "\n";
+            list = list.concat(temp);
+        }
+        return list;
+    }
 
-		for(int i = 0; i < numItems; i++) {
-			if(iArray[i].isMemberHere(id) == true) {
-				list.concat(iArray[i].getID());
-			}
-		}
+    public Boolean addMemberToItem(String memberID, String itemID) {
+        if(this.type == itemListType.Library) {
+            return false;
+        }
+        else {
+            for(int i = 0; i < numItems; i++) {
+                if(iArray[i].getID().compareTo(itemID) == 0){
+                    Boolean success = iArray[i].addMember(memberID);
+                    if(success == false){
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
 
-		return list;
-	}
-	
-	public String toString() {
-		String list = "";
-		for(int i = 0; i < numItems; i++) {
-			String temp = iArray[i].toString() + "\n";
-			list = list.concat(temp);
-		}
-		return list;
-	}
+    public Boolean removeMemberFromItem(String memberID, String itemID) {
+        if(this.type == itemListType.Library) {
+            return false;
+        }
+        else {
+            for(int i = 0; i < numItems; i++) {
+                if(iArray[i].getID().compareTo(itemID) == 0){
+                    Boolean success = iArray[i].removeMember(memberID);
+                    if(success == false) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+    
+    public void addItem(String t, String y, String a, int q) {
+        Item temp = new Item(t, y, a, q);
+        
+        if(numItems == iArray.length) {
+            Item[] newArray = new Item[numItems *2];
+            for(int i = 0; i < (numItems*2); i++) {
+                newArray[i] = iArray[i];
+            }
+            iArray = newArray;
+        }
+        iArray[numItems] = temp;
+        numItems++;
+        modified = true;
+    }
+    
+    public itemListType getType() {
+        return type;
+    }
+    
+    public int getNumItems() {
+        return numItems;
+    }
 
-	// If list is of type library you can not add owners to it
-	// Get memberID and ItemID, look for the matching itemID
-	// if no matches or item is out of copies return false
-	// otherwise return true
-	public Boolean addMemberToItem(String memberID, String itemID) {
-		if(this.type == itemListType.Library) {
-			return false;
-		}
-		else {
-			for(int i = 0; i < numItems; i++) {
-				if(iArray[i].getID().compareTo(itemID) == 0){
-					Boolean success = iArray[i].addMember(memberID);
-					if(success == false){
-						return false;
-					}
-				}
-			}
-			return true;
-		}
-	}
+    public Item[] getIArray() {
+        return Arrays.copyOf(iArray, numItems); // Safe copy
+    }
+    
+    public void removeItem(String id) {
+        for(int i = 0; i < numItems; i++) {
+            if(iArray[i].getID().compareTo(id) == 0) {
+                iArray[i] = iArray[numItems];
+                numItems--;
+            }
+        }
+    }
+    
+    public String getTitle(String bookID) {
+        for(int i = 0; i < numItems; i++) {
+            if(iArray[i].getID().compareTo(bookID) == 0) {
+                return iArray[i].getTitle();
+            }
+        }
+        return "Book not found";
+    }
 
-	// Same thing as add member to Item but remove instead 
-	public Boolean removeMemberFromItem(String memberID, String itemID) {
-		if(this.type == itemListType.Library) {
-			return false;
-		}
-		else {
-			for(int i = 0; i < numItems; i++) {
-				if(iArray[i].getID().compareTo(itemID) == 0){
-					Boolean success = iArray[i].removeMember(memberID);
-					if(success == false) {
-						return false;
-					}
-				}
-			}
-			return true;
-		}
-	}
-	
-	// Add an item with title, year, author, quantity 
-	public void addItem(String t, String y, String a, int q) {
-		Item temp = new Item(t, y, a, q);
-		
-		if(numItems == iArray.length) {
-			Item[] newArray = new Item[numItems *2];
-			for(int i = 0; i < (numItems*2); i++) {
-				newArray[i] = iArray[i];
-			}
-			iArray = newArray;
-		}
-		iArray[numItems] = temp;
-		numItems++;
-		modified = true;
-	}
-	
-	// Get the type of list this is
-	public itemListType getType() {
-		return type;
-	}
-	
-	public void removeItem(String id) {
-		
-		for(int i = 0; i < numItems; i++) {
-			if(iArray[i].getID().compareTo(id) == 0) {
-				iArray[i] = iArray[numItems];
-				numItems--;
-			}
-		}
-	}
-	
-	public String getTitle(String bookID) {
-		for(int i = 0; i < numItems; i++) {
-			if(iArray[i].getID().compareTo(bookID) == 0) {
-				return iArray[i].getTitle();
-			}
-		}
-		return "Book not found";
-	}
+    public String getYear(String bookID) {
+        for(int i = 0; i < numItems; i++) {
+            if(iArray[i].getID().compareTo(bookID) == 0) {
+                return iArray[i].getYear();
+            }
+        }
+        return "Book not found";
+    }
+    
+    public String getAuthor(String bookID) {
+        for(int i = 0; i < numItems; i++) {
+            if(iArray[i].getID().compareTo(bookID) == 0) {
+                return iArray[i].getAuthor();
+            }
+        }
+        return "Book not found";
+    }
 
-	public String getYear(String bookID) {
-		for(int i = 0; i < numItems; i++) {
-			if(iArray[i].getID().compareTo(bookID) == 0) {
-				return iArray[i].getYear();
-			}
-		}
-		return "Book not found";
-	}
-	
-	public String getAuthor(String bookID) {
-		for(int i = 0; i < numItems; i++) {
-			if(iArray[i].getID().compareTo(bookID) == 0) {
-				return iArray[i].getAuthor();
-			}
-		}
-		return "Book not found";
-	}
-
-	public void save() {
-		if(modified == false) {
-			return;
-		}
-		
-		try {
-			FileWriter out = new FileWriter(sourceName);
-			
-			for(int i = 0; i < numItems; i++) {
-				out.write(iArray.toString() + "\n");
-			}
-			
-			out.close();
-			
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public boolean load() {
-		// Save writes out the attributes seperated by comma and the number of owners + list of members
-		// number of owners is important because we need to somehow know the number of strings to parse
-		// while saving or loading.
-		
-		File file = new File(sourceName);
-		try {
-			if(file.createNewFile()) {
-				return false;
-			}
-			
-			Scanner scanner = new Scanner(file);
-			scanner.useDelimiter(",|\n");
-			
-			// Format of each item 
-			// itemID + "," + title + "," + year + "," + author + "," + quantity + "," + numOwners + "," ownerList[] 
-			while(scanner.hasNext()) {
-				String id = scanner.next();
-				String title = scanner.next();
-				String year = scanner.next();
-				String author = scanner.next();
-				//I don't know if this works? 
-				Integer quantity = Integer.valueOf(scanner.next());
-				Item temp = new Item(id, title, year, author, quantity);
-				
-				Integer numOwners = Integer.valueOf(scanner.next());
-				
-				// Get the number of owners, repeatedly scan and call addOwner until done then repeat the process
-				// for each item
-				for(int i = 0; i < numOwners; i++) {
-					String scan = scanner.next();
-					temp.addMember(scan);
-				}
-				scanner.close();
-				return true;
-			}
-		}
-		catch (Exception e) {
-			System.out.println("Error reading file");
-		}
-		//False return in case item loading was successful but it's an empty file
-		return false;
-	}
-	
+    public void save() {
+        if(modified == false) {
+            return;
+        }
+        
+        try {
+            FileWriter out = new FileWriter(sourceName);
+            
+            for(int i = 0; i < numItems; i++) {
+                out.write(iArray.toString() + "\n");
+            }
+            
+            out.close();
+            
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public boolean load() {
+        File file = new File(sourceName);
+        try {
+            if(file.createNewFile()) {
+                return false;
+            }
+            
+            Scanner scanner = new Scanner(file);
+            scanner.useDelimiter(",|\n");
+            
+            while(scanner.hasNext()) {
+                String id = scanner.next();
+                String title = scanner.next();
+                String year = scanner.next();
+                String author = scanner.next();
+                Integer quantity = Integer.valueOf(scanner.next());
+                Item temp = new Item(id, title, year, author, quantity);
+                
+                Integer numOwners = Integer.valueOf(scanner.next());
+                
+                for(int i = 0; i < numOwners; i++) {
+                    String scan = scanner.next();
+                    temp.addMember(scan);
+                }
+                scanner.close();
+                return true;
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Error reading file");
+        }
+        return false;
+    }
 }
